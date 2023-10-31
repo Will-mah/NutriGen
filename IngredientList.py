a170f85fa8c4ca3afb51d5c2fe6dd28a29a08be7
Will-Mah <wilmaop99@gmail.com>
2023-10-31 10:35:56 +0000

    file restructuring

--- IngredientList.py
+++ IngredientList.py
@@ -25,45 +25,46 @@ print(ingredient_list)
 
 # Mapping of Unicode fraction characters to their string equivalents
 unicode_fraction_mapping = {
-    '\u00BC': ' 1/4',
-    '\u00BD': ' 1/2',
-    '\u00BE': ' 3/4',
-    '\u2150': ' 1/7',
-    '\u2151': ' 1/9',
-    '\u2152': ' 1/10',
-    '\u2153': ' 1/3',
-    '\u2154': ' 2/3',
-    '\u2155': ' 1/5',
-    '\u2156': ' 2/5',
-    '\u2157': ' 3/5',
-    '\u2158': ' 4/5',
-    '\u2159': ' 1/6',
-    '\u215A': ' 5/6',
-    '\u215B': ' 1/8',
-    '\u215C': ' 3/8',
-    '\u215D': ' 5/8',
-    '\u215E': ' 7/8',
+    '\u00BC': '1/4',
+    '\u00BD': '1/2',
+    '\u00BE': '3/4',
+    '\u2150': '1/7',
+    '\u2151': '1/9',
+    '\u2152': '1/10',
+    '\u2153': '1/3',
+    '\u2154': '2/3',
+    '\u2155': '1/5',
+    '\u2156': '2/5',
+    '\u2157': '3/5',
+    '\u2158': '4/5',
+    '\u2159': '1/6',
+    '\u215A': '5/6',
+    '\u215B': '1/8',
+    '\u215C': '3/8',
+    '\u215D': '5/8',
+    '\u215E': '7/8',
  }
 
 ingredient_pattern = re.compile(
-        r'('
-        r'\b\d*[\s]*'  # Matches the optional whole number part of the quantity
-        r'(?:\d+/\d+|[\u00BC-\u00BE\u2150-\u215E])?'  # Matches fractional part of the quantity (including unicode fractions)
-        r'[\s]*'  # Matches any spaces following the numbers
-        r'(?:fl\.?\s?)?'  # Optionally matches 'fl' abbreviation for fluid measurements
-        r'(?:g|oz|lbs?|kg|tbsp?|tsp|cups?|ml|l|pints?|quarts?|gallons?|litres?|liters?)'  # Matches the unit including long-form like 'litres'
-        r'[\s\/]*'  # Matches spaces or slashes after the unit
-        r'(?:\d*[\s]*'  # Matches an optional whole number for alternative measurements
-        r'(?:\d+/\d+|[\u00BC-\u00BE\u2150-\u215E])?'  # Matches fractional part of the alternative measurements (including unicode fractions)
-        r'[\s]*'  # Matches any spaces following the alternative measurements
-        r'(?:fl\.?\s?)?'  # Optionally matches 'fl' abbreviation for fluid measurements
-        r'(?:g|oz|lbs?|kg|tbsp?|tsp|cups?|ml|l|pints?|quarts?|gallons?|litres?|liters?)?'  # Optionally matches the unit for alternative measurements including long-form
-        r')?'  # Makes the entire alternative measurements part optional
-        r'\b\.?'  # Matches a word boundary and an optional period
-        r')'
-        r'(\s.*$)',  # The rest is the ingredient name
-        re.IGNORECASE
-    )
+    r'('
+    r'\b\d*[\s]*'  # Matches the optional whole number part of the quantity
+    r'(?:\d+\s*\d*/\d+|\d*/\d+|\s*[\u00BC-\u00BE\u2150-\u215E])?'  # Matches whole plus fractional part of the quantity (including unicode fractions and spaces)
+    r'[\s]*'  # Matches any spaces following the numbers
+    r'(?:fl\.?\s?)?'  # Optionally matches 'fl' abbreviation for fluid measurements
+    r'(?:g|oz|lbs?|kg|tbsp?|tsp|cups?|ml|l|pints?|quarts?|gallons?|litres?|liters?)'  # Matches the unit
+    r'[\s\/]*'  # Matches spaces or slashes after the unit
+    r'(?:\d*[\s]*'  # Matches an optional whole number for alternative measurements
+    r'(?:\d+/\d+|\s*[\u00BC-\u00BE\u2150-\u215E])?'  # Matches fractional part of the alternative measurements (including unicode fractions)
+    r'[\s]*'  # Matches any spaces following the alternative measurements
+    r'(?:fl\.?\s?)?'  # Optionally matches 'fl' abbreviation for fluid measurements
+    r'(?:g|oz|lbs?|kg|tbsp?|tsp|cups?|ml|l|pints?|quarts?|gallons?|litres?|liters?)?'  # Optionally matches the unit for alternative measurements
+    r')?'  # Makes the entire alternative measurements part optional
+    r'\b\.?'  # Matches a word boundary and an optional period
+    r')'
+    r'(\s.*$)',  # The rest is the ingredient name
+    re.IGNORECASE
+)
+
 
 def convert_unicode_fractions(ingredient):
     for unicode_fraction, string_fraction in unicode_fraction_mapping.items():
@@ -106,16 +107,16 @@ def split_ingredient(ingredient):
 
     return quantity, name
 
-# Test the function
-# ingredient_list = [
-#     '75g/2½oz self-raising flour',
-#     '1 cup/240ml milk',
-#     '1/2 tbsp vanilla extract',
-#     '140g/5oz butter, softened',
-#     '4 eggs',
-#     '1 litre/1¾ pints vegetable stock',
-#     '½ tsp salt'  # Added this test case
-# ]
+#Test the function
+ingredient_list = [
+    '75g/2½oz self-raising flour',
+    '1 cup/240ml milk',
+    '1/2 tbsp vanilla extract',
+    '140g/5oz butter, softened',
+    '4 eggs',
+    '1 litre/1¾ pints vegetable stock',
+    '½ tsp salt'  # Added this test case
+]
 
 
 split_ingredients = [split_ingredient(ingredient) for ingredient in ingredient_list]
