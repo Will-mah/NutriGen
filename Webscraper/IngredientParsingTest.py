33d8b45e2ab5fbfcb4b5ac61bef79e1e09fb4aee
Will-Mah <wilmaop99@gmail.com>
2023-10-31 10:38:41 +0000

    file restructuring again

--- Webscraper/IngredientParsingTest.py
+++ Webscraper/IngredientParsingTest.py
@@ -1,11 +1,11 @@
 import unittest
-from IngredientList import split_ingredient  # make sure to replace 'your_script' with the actual name of your Python file containing the split_ingredient function.
+from IngredientList import split_ingredient  
 
 class TestSplitIngredient(unittest.TestCase):
 
     def test_regular_measurement(self):
         ingredient = '75g/2½oz self-raising flour'
-        expected_output = ('75g/2½oz', 'Self-Raising Flour')
+        expected_output = ('75g/21/2oz', 'Self-raising Flour')
         self.assertEqual(split_ingredient(ingredient), expected_output)
 
     def test_ingredient_with_standalone_number(self):
@@ -15,17 +15,17 @@ class TestSplitIngredient(unittest.TestCase):
 
     def test_ingredient_with_longform_unit(self):
         ingredient = '1 litre/1¾ pints vegetable stock'
-        expected_output = ('1 litre/1¾ pints', 'Vegetable Stock')
+        expected_output = ('1 litre/13/4 pints', 'Vegetable Stock')
         self.assertEqual(split_ingredient(ingredient), expected_output)
 
     def test_ingredient_with_unicode_fraction(self):
         ingredient = '½ tsp salt'
-        expected_output = ('½ tsp', 'Salt')
+        expected_output = ('1/2 tsp', 'Salt')
         self.assertEqual(split_ingredient(ingredient), expected_output)
 
     def test_ingredient_with_unknown_quantity(self):
         ingredient = 'some olive oil'
-        expected_output = ('Unknown quantity', 'Olive Oil')
+        expected_output = ('Unknown quantity', 'Some Olive Oil')
         self.assertEqual(split_ingredient(ingredient), expected_output)
 
     # Add more tests for edge cases and different scenarios...
